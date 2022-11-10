2ac7c0037227d666324861b8bc176ceed85e5399
Pablo <pbarrn00@estudiantes.unileon.es>
2022-11-10 21:08:44 +0100

    avances practica cowrie

--- dmz/start.sh
+++ dmz/start.sh
@@ -4,4 +4,7 @@ route add default gw 10.5.1.1
 
 route del default gw 10.5.1.254
 
+sudo iptables -t nat -A PREROUTING -p tcp --dport 22 -j REDIRECT --to-port 2222
+#sudo iptables -t nat -A PREROUTING -p tcp --dport 23 -j REDIRECT --to-port 2223
+ 
 /usr/sbin/sshd -D
--- fw/start.sh
+++ fw/start.sh
@@ -11,5 +11,7 @@ iptables -P FORWARD DROP
 # Permitir trafico de conexiones establecidas o relacionadas para el protocolo TCP
 iptables -A FORWARD -p tcp -m state --state ESTABLISHED,RELATED -j ACCEPT
 
+#snort -c /usr/local/etc/snort/snort.lua -R /usr/local/etc/rules/snort3-community.rules
+#snort -c /usr/local/etc/snort/snort.lua -R /usr/local/etc/rules/snort3-community.rules -i eth1 -A full -l /var/log/snort
 
 /usr/sbin/sshd -D
