4b1a7618517f87980f9e4f3fa5b0627efa3c6b5d
pbarrn00 <pbarrn00@estudiantes.unileon.es>
2022-11-11 02:21:02 +0100

    avances

--- fw/start.sh
+++ fw/start.sh
@@ -11,6 +11,8 @@ iptables -P FORWARD DROP
 # Permitir trafico de conexiones establecidas o relacionadas para el protocolo TCP
 iptables -A FORWARD -p tcp -m state --state ESTABLISHED,RELATED -j ACCEPT
 
+iptables -A FORWARD -p tcp -i eth1 -o eth0 -s 10.5.0.0/24 -d 10.5.1.0/24 -m state --state NEW -j ACCEPT
+
 #snort -c /usr/local/etc/snort/snort.lua -R /usr/local/etc/rules/snort3-community.rules
 #snort -c /usr/local/etc/snort/snort.lua -R /usr/local/etc/rules/snort3-community.rules -i eth1 -A full -l /var/log/snort
 
